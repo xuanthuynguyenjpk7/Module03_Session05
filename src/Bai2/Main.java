0642f53c6ab51aa7f2368738d9d735c8fc4f5c10
xuant <xuanthuynguyenjpk7@gmail.com>
2026-08-19 01:59:23 +0900

    add bài tập

--- src/Bai2/Main.java
+++ src/Bai2/Main.java
@@ -1,4 +1,23 @@
 package Bai2;
-
 public class Main {
+    public static void main(String[] args) {
+
+        // Chuỗi ban đầu
+        StringBuilder sb = new StringBuilder("Hello, Java World!");
+
+        // In chuỗi ban đầu
+        System.out.println("Chuỗi ban đầu: " + sb);
+
+        // Xóa các ký tự từ vị trí 5 đến 9
+        sb.delete(5, 10);
+
+        // In chuỗi sau khi xóa
+        System.out.println("Chuỗi sau khi xóa: " + sb);
+
+        // Thay thế "World" bằng "Universe"
+        sb.replace(7, 12, "Universe");
+
+        // In chuỗi sau khi thay thế
+        System.out.println("Chuỗi sau khi thay thế: " + sb);
+    }
 }
